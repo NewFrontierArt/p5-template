e07d02788dd57bab8a4e61cd500104702f619fa4
Arthur Tavares <arthur001123@gmail.com>
2024-02-07 12:17:59 -0300

    use nfaRandom as seed instead of nfaRandom in every instance

--- sketch.js
+++ sketch.js
@@ -9,46 +9,55 @@ function setup() {
 }
 
 function draw() {
-    noLoop();   //noLoop tells p5.js to stop rendering the image after it's completed (only animations wouldn't want this)
-    background(0);
-    noFill();
+  const seed = nfaRandom(0, 4294967295); // Sets up the subsequent random() and noise() functions to have deterministic outputs
+  console.log("seed is", seed);
+  randomSeed(seed);
+  noiseSeed(seed);
 
-    //Define circle colors and shadows
-    stroke(255);
-    d = displayDensity();
-    ctx = drawingContext;
-    ctx.shadowColor = "#fff";
-    ctx.shadowBlur = 20;
+  noLoop(); //noLoop tells p5.js to stop rendering the image after it's completed (only animations wouldn't want this)
+  background(0);
+  noFill();
 
-    let circlesPerRow = nfaRandom(1, 30); // Generates a pseudorandom number between 1 and 16 using the NFT mint address as seed
+  console.log(random(0, 5));
 
-  let totalRadius = 0;  
-  
-    // Creates a matrix with circles
-    for (let i = 0; i < circlesPerRow; i++) {
-        for (let j = 0; j < circlesPerRow; j++) {
-          strokeWeight(nfaRandom(1, 5)); // The circles stroke weight will be randomized from 1 to 5
+  //Define circle colors and shadows
+  stroke(255);
+  d = displayDensity();
+  ctx = drawingContext;
+  ctx.shadowColor = "#fff";
+  ctx.shadowBlur = 20;
 
-          // circle position is set as it's postion in the i/j matrix
-          let x = (width / circlesPerRow) * i + width / (circlesPerRow * 2);
-          let y = (height / circlesPerRow) * j + height / (circlesPerRow * 2);
+  let circlesPerRow = int(random(1, 30)); // Generates a pseudorandom number between 1 and 30
 
-          // each circle's size is randomized in a range based in the total amount of circles
-          let circleRadius = nfaRandom(
-            size / 6 / circlesPerRow,
-            (size * 1) / circlesPerRow
-          );
+  console.log("circles", circlesPerRow);
 
-          totalRadius += circleRadius
+  let totalRadius = 0;
 
-          circle(x, y, circleRadius); // draw the circle 
-        }
+  // Creates a matrix with circles
+  for (let i = 0; i < circlesPerRow; i++) {
+    for (let j = 0; j < circlesPerRow; j++) {
+      strokeWeight(random(1, 5)); // The circles stroke weight will be randomized from 1 to 5
+
+      // circle position is set as it's postion in the i/j matrix
+      let x = (width / circlesPerRow) * i + width / (circlesPerRow * 2);
+      let y = (height / circlesPerRow) * j + height / (circlesPerRow * 2);
+
+      // each circle's size is randomized in a range based in the total amount of circles
+      let circleRadius = random(
+        size / 6 / circlesPerRow,
+        (size * 1) / circlesPerRow
+      );
+
+      totalRadius += circleRadius;
+
+      circle(x, y, circleRadius); // draw the circle
     }
+  }
 
   // Calculate NFT attributes
   const totalCircles = circlesPerRow * circlesPerRow;
   const circlesSize = totalRadius / totalCircles > 20 ? "big" : "small";
-  
+
   // Tell the NFA system to finish the NFT generation with it's traits
   nfaFinish([
     {
